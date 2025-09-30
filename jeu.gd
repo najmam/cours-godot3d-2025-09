1db38757ed20d8066a4aec5fbb0252c7e27ab435
Naji Mammeri <bonjour@njmm.fr>
2025-09-30 11:20:09 +0200

    code de déplacement plus court

--- jeu.gd
+++ jeu.gd
@@ -7,27 +7,13 @@ func _ready():
 
 func _process(delta: float) -> void:
 	# print("temps écoulé depuis la dernière frame : ", delta)
-	var personnage = get_node("character") as Node3D
+	var personnage = $character
 	
 	var vitesse = Vector3(0,0,0)
-	
-	var should_go_up = Input.is_action_pressed("player_up")
-	var should_go_right = Input.is_action_pressed("player_right")
-	var should_go_left = Input.is_action_pressed("player_left")
-	var should_go_down = Input.is_action_pressed("player_down")
-	if should_go_up:
-		vitesse += Vector3(0,0,-1)
-	if should_go_down:
-		vitesse += Vector3(0,0,1)
-	if should_go_left:
-		vitesse += Vector3(-1,0,0)
-	if should_go_right:
-		vitesse += Vector3(1,0,0)
+	var input_lr = Input.get_axis("player_left", "player_right")
+	vitesse += Vector3(1,0,0) * input_lr
+	var input_ud = Input.get_axis("player_up", "player_down")
+	vitesse += Vector3(0,0,1) * input_ud
 	
 	var deplacement = VALEUR_VITESSE * vitesse * delta
 	personnage.position = personnage.position + deplacement
-	
-	# Z: avant
-	# Q: gauche
-	# D: droite
-	# S: arrière
