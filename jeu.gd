d71be5ce1a4033039bebcc21e9606efdf544c762
Naji Mammeri <bonjour@njmm.fr>
2025-10-06 22:00:31 +0200

    jour 1 exo 4 : contrôle caméra

--- jeu.gd
+++ jeu.gd
@@ -1,7 +1,14 @@
 extends Node3D
 
 const VITESSE_MAX = 3.5
-var orientation: float = 0
+var orientation: float = 0 
+
+# préférences de contrôle de la caméra :
+# ici préférences perso du prof,
+# changez à votre convenance
+# ou rendez ça paramétrable (exercice bonus !)
+var cam_invert_horizontal = 1. # ne pas inverser
+var cam_invert_vertical = -1. # inverser
 
 func _ready():
 	pass
@@ -27,9 +34,18 @@ func _process(delta: float) -> void:
 		orientation = (-Vector3.FORWARD).signed_angle_to(vitesse, Vector3.UP)
 	%character.rotation.y = orientation
 	
+	
 	## EXO 3
 	## - permettre de déplacer le personnage à la manette
+	# solution: voir l'input map dans les paramètres du projet
 	
 	## EXO BONUS:
 	## - permettre de déplacer la caméra à la manette
 	##   tout en conservant un déplacement du personnage qui fonctionne 
+	# rotation de la caméra dans son repère local,
+	# en suivant l'ordre des rotations (paramètre rotation order)
+	var cam_input = Vector3(0,1,0) * Input.get_axis("camera_left", "camera_right") \
+		+ Vector3(1,0,0) * Input.get_axis("camera_down", "camera_up")
+	var cam_rotation_delta = cam_input * delta * Vector3(1,1,1)
+	%camera.rotation.x += cam_rotation_delta.x * cam_invert_vertical
+	%camera.global_rotation.y += cam_rotation_delta.y * cam_invert_horizontal
