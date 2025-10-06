1c24b598f149d1a72c2541ee550f1f0371b31f85
Naji Mammeri <bonjour@njmm.fr>
2025-10-06 21:40:12 +0200

    cours 1 exo 2 : perso regarde où il va

--- jeu.gd
+++ jeu.gd
@@ -1,33 +1,31 @@
 extends Node3D
 
-var VALEUR_VITESSE = 3.5
+const VITESSE_MAX = 3.5
+var orientation: float = 0
 
 func _ready():
-	print("coucou")
+	pass
 
 func _process(delta: float) -> void:
-	# print("temps écoulé depuis la dernière frame : ", delta)	
 	var personnage = %character
 	
-	var vitesse = Vector3(0,0,0)
+	var vitesse = Vector3.ZERO
 	var input_lr = Input.get_axis("player_left", "player_right")
-	vitesse += Vector3(1,0,0) * input_lr
-	var input_ud = Input.get_axis("player_up", "player_down")
-	vitesse += Vector3(0,0,1) * input_ud
+	vitesse += Vector3.RIGHT * input_lr
+	var input_ud = Input.get_axis("player_down", "player_up")
+	vitesse += Vector3.FORWARD * input_ud
 	
-	var deplacement = VALEUR_VITESSE * vitesse * delta
+	var deplacement = VITESSE_MAX * vitesse * delta
 	personnage.position = personnage.position + deplacement
 	
-	var orientation = 0
 	## EXO 2
 	## BUT DU JEU :
 	## 1) le perso doit regarder où il va
 	## 2) quand on n'appuie sur rien, le perso doit garder la dernière
 	##    direction de regard qu'il avait pendant qu'il se déplaçait.
-	var machin = vitesse.angle_to(Vector3(0,0,1)) * 180/PI
-	print("vitesse = ", vitesse, ", machin = ", machin)
-	orientation = machin
-	%character.rotation_degrees.y = orientation
+	if vitesse != Vector3.ZERO:
+		orientation = (-Vector3.FORWARD).signed_angle_to(vitesse, Vector3.UP)
+	%character.rotation.y = orientation
 	
 	## EXO 3
 	## - permettre de déplacer le personnage à la manette
